2b7bfb908b4db4a7f1d52ef4b748ee85c09a2de0
zhangshuisheng <zhangshuisheng@rd.tuan800.com>
2017-02-13 17:24:46 +0800

    添加文件

---
+++ android/app/src/main/java/com/thirtydaysofreactnative/MainActivity.java
@@ -0,0 +1,18 @@
+package com.thirtydaysofreactnative;
+
+import com.facebook.react.ReactActivity;
+import com.airbnb.android.react.maps.MapsPackage;
+import com.brentvatne.react.ReactVideoPackage;
+import com.oblador.vectoricons.VectorIconsPackage;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "ThirtyDaysOfReactNative";
+    }
+}
---
+++ ios/Pods/Target Support Files/Pods-ThirtyDaysOfReactNative/Pods-ThirtyDaysOfReactNative-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ThirtyDaysOfReactNative : NSObject
+@end
+@implementation PodsDummy_Pods_ThirtyDaysOfReactNative
+@end
